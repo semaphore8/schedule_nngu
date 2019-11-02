afa22a4f28af8a5e2968d260e48d68fff695c606
sgbliznyuk <sgbliznyuk@compcenter.org>
2019-11-02 11:41:32 +0300

    added html layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,127 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './Body.css';
+import './Footer.css';
+import './Header.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div>  
+    <div className="Header">
+      <div className="Hat">
+        🎓
+      </div>
+      <div className="Title">
+        Расписание
+      </div>
+      <div className="Today-button">
+      <button>Сегодня</button>
+      </div>
+      <div className="Arrow">
+        <button>&#60;</button>
+      </div>
+      <div className="Arrow">
+        <button>&#62;</button>
+      </div>
+      <div className="Months">
+        Октябрь 2019
+      </div>
+      <div className="Dropdown">
+        <button>
+          Группа <span>&#9660;</span>
+        </button>
+      </div>
+      <div className="Dropdown">
+        <button>
+          Неделя <span>&#9660;</span>
+        </button>
+      </div>
+      <div className="Nngu">
+        Аф ННГУ им. Н.И. Лобачевского
+      </div>
     </div>
+    <div className="Schedule-wrapper">
+      <div className="Schedule-row">
+        <div className="Schedule-cell" id="left">
+
+        </div>
+        <div className="Schedule-cell"></div>
+        <div className="Schedule-cell"></div>
+        <div className="Schedule-cell"></div>
+        <div className="Schedule-cell"></div>
+        <div className="Schedule-cell"></div>
+        <div className="Schedule-cell"></div>
+      </div>
+      <div className="Schedule-row">
+        <div className="Schedule-cell" id="left"><span>
+        <big>I</big> 8:<small>00</small> - 9:<small>35</small></span>
+        </div>
+        <div className="Schedule-cell">
+        Речевые практики (ПЗ)<br />
+        доц. С.В. Зотова<br />
+        (ауд.437)
+        </div>
+        <div className="Schedule-cell"></div>
+        <div className="Schedule-cell"></div>
+        <div className="Schedule-cell"></div>
+        <div className="Schedule-cell"></div>
+        <div className="Schedule-cell"></div>
+      </div>
+      <div className="Schedule-row">
+        <div className="Schedule-cell" id="left"><span>
+        <big>II</big> 9:<small>45</small> - 11:<small>20</small></span></div>
+        <div className="Schedule-cell"></div>
+        <div className="Schedule-cell"></div>
+        <div className="Schedule-cell"></div>
+        <div className="Schedule-cell"></div>
+        <div className="Schedule-cell"></div>
+        <div className="Schedule-cell"></div>
+      </div>
+      <div className="Schedule-row">
+        <div className="Schedule-cell" id="left"><span>
+        <big>III</big> 12:<small>00</small> - 13:<small>35</small></span></div>
+        <div className="Schedule-cell"></div>
+        <div className="Schedule-cell"></div>
+        <div className="Schedule-cell"></div>
+        <div className="Schedule-cell"></div>
+        <div className="Schedule-cell"></div>
+        <div className="Schedule-cell"></div>
+      </div>
+      <div className="Schedule-row">
+        <div className="Schedule-cell" id="left"><span>
+        <big>IV</big> 13:<small>45</small> - 15:<small>20</small></span></div>
+        <div className="Schedule-cell"></div>
+        <div className="Schedule-cell"></div>
+        <div className="Schedule-cell"></div>
+        <div className="Schedule-cell"></div>
+        <div className="Schedule-cell"></div>
+        <div className="Schedule-cell"></div>
+      </div>
+      <div className="Schedule-row">
+        <div className="Schedule-cell" id="left"><span>
+        <big>V</big> 15:<small>30</small> - 17:<small>05</small></span></div>
+        <div className="Schedule-cell"></div>
+        <div className="Schedule-cell"></div>
+        <div className="Schedule-cell"></div>
+        <div className="Schedule-cell"></div>
+        <div className="Schedule-cell"></div>
+        <div className="Schedule-cell"></div>
+      </div>
+      <div className="Schedule-row" id="bottom">
+        <div className="Schedule-cell" id="left"><span>
+        <big>VI</big> 17:<small>15</small> - 18:<small>50</small></span></div>
+        <div className="Schedule-cell"></div>
+        <div className="Schedule-cell"></div>
+        <div className="Schedule-cell"></div>
+        <div className="Schedule-cell"></div>
+        <div className="Schedule-cell"></div>
+        <div className="Schedule-cell"></div>
+      </div>
+    </div>
+    <div className="Footer">
+      <a href="https://github.com/sgbliznyuk">© 2019 Simon B 🧐</a>
+    </div>
+  </div>
   );
 }
 
