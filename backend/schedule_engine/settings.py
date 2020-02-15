c5bae9bd2be2425cdbe850f19aec5bad976d4865
sgbliznyuk <sgbliznyuk@gmail.com>
2020-02-15 14:21:05 +0300

    changed cors settings to allow only get

--- backend/schedule_engine/settings.py
+++ backend/schedule_engine/settings.py
@@ -138,16 +138,19 @@ CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000',
 ]
 
-# CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_ALLOW_ALL = False
 
 CORS_ALLOW_METHODS = [
-    'DELETE',
     'GET',
-    'OPTIONS',
-    'PATCH',
-    'POST',
-    'PUT',
 ]
+# CORS_ALLOW_METHODS = [
+#     'DELETE',
+#     'GET',
+#     'OPTIONS',
+#     'PATCH',
+#     'POST',
+#     'PUT',
+# ]
 
 # date format in admin
 
